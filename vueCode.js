ba728212a10d9f783bc8df1cc96db192e0c420ad
FireLemons <shenyang64@gmail.com>
2018-05-19 15:55:51 -0500

    learning modules

--- vueCode.js
+++ vueCode.js
@@ -1,21 +1,33 @@
-new Vue({
-  el: '#app',
+var template = new Vue({
+  el: '#template',
   data: {
     message: 'Hello Vue.js!',
-    seen: {
-    	nested: 1
-    },
-    todos: [
-      {
-      	text: 'Learn JS'
-      },
-      {
-      	text: 'Learn Vue'
-      },
-      {
-      	text: 'Build something modern'
-      }
-    ]
   },
   methods:{}
-})
+});
+
+var attribute = new Vue({
+	el: '#attribute', 
+	data:{
+		message: 'HOVER HINT'
+  }
+});
+
+var control = new Vue({
+	el: '#control',
+	data: {
+  	seen: 1,
+    unseen: 0,
+  	todos: [
+  		{
+  	    text: 'Learn JS'
+  	  },
+  	  {
+  	    text: 'Learn Vue'
+  	  },
+  	  {
+  	    text: 'Build something modern'
+  	  }
+  	]
+  }
+});
